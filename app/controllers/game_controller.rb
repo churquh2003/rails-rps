67bb7bdb97b5f334a2432739bd0bd06801016e86
churquh2003 <curquhart@uchicago.edu>
2024-11-06 00:00:38 +0000

    new

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,4 +1,4 @@
-
+# app/controllers/game_controller.rb
 class GameController < ApplicationController
   def home
   end
@@ -6,28 +6,29 @@ class GameController < ApplicationController
   def rock
     @user_choice = "rock"
     @computer_choice = ["rock", "paper", "scissors"].sample
-    @result = "We " + determine_winner(@user_choice, @computer_choice)
+    @result = determine_winner(@user_choice, @computer_choice)
   end
 
   def paper
     @user_choice = "paper"
     @computer_choice = ["rock", "paper", "scissors"].sample
-    @result = "We " + determine_winner(@user_choice, @computer_choice)
+    @result = determine_winner(@user_choice, @computer_choice)
   end
 
   def scissors
     @user_choice = "scissors"
     @computer_choice = ["rock", "paper", "scissors"].sample
-    @result = "We " + determine_winner(@user_choice, @computer_choice)
+    @result = determine_winner(@user_choice, @computer_choice)
   end
 
   private
 
   def determine_winner(user, computer)
-    return "tie" if user == computer
-    return "win" if (user == "rock" && computer == "scissors") ||
-                     (user == "paper" && computer == "rock") ||
-                     (user == "scissors" && computer == "paper")
-    "lose"
+    return "We tie" if user == computer
+    return "We win" if (user == "rock" && computer == "scissors") ||
+                       (user == "paper" && computer == "rock") ||
+                       (user == "scissors" && computer == "paper")
+    "We lose"
   end
 end
+
